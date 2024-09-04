3abfc84f0246e5c134423f046b62a21b8d05ecba
Aslam <93756533+LW1117@users.noreply.github.com>
2024-09-04 14:08:09 +0530

    Add due functions

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -10,21 +10,40 @@ const todoList = () => {
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
+    return all.filter((todoItem) => {
+      if (todoItem.dueDate <= yesterday) return true;
+      else return false;
+    });
   };
 
   const dueToday = () => {
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
+    return all.filter((todoItem) => {
+      if (todoItem.dueDate === today) return true;
+      else return false;
+    });
   };
 
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
+    return all.filter((todoItem) => {
+      if (todoItem.dueDate >= tomorrow) return true;
+      else return false;
+    });
   };
 
   const toDisplayableList = (list) => {
     // Format the To-Do list here, and return the output string
     // as per the format given above.
+    formattedList = ``;
+    list.forEach((item) => {
+      formattedList += `[${item.completed ? "x" : " "}] ${item.title} ${
+        item.dueDate === today ? "" : item.dueDate
+      }\n`;
+    });
+    return formattedList;
   };
 
   return {
