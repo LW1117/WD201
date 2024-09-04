eb4e331d5c7d28da37edfb4818390e3df3580331
Aslam <93756533+LW1117@users.noreply.github.com>
2024-09-04 11:09:43 +0530

    Change js to create a  txt file

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,15 @@
-let hello = () => {
-  console.log("Hello GitHub!");
-};
+const fs = require("fs");
 
-hello();
+fs.writeFile(
+  "sample.txt",
+  "Hellow World. Welcome to Node.js file System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+
+fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
